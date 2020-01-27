58c2e1b6634c739b8552e667779d2c8e0b3db884
ydh0520 <ydh2338@naver.com>
2020-01-28 02:00:07 +0900

    java_template

---
+++ SW Expert Academy/JAVA_Template.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) throws Exception{
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuffer sb = new StringBuffer();
+    
+    int tc = Integer.parseInt(bf.readLine());
+    
+    for(int test_case = 1;test_case<=tc;test_case++) {
+        st = new StringTokenizer(bf.readLine());
+    }	
+    System.out.println(sb.toString());
+}
